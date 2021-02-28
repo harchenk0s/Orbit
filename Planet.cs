67355ddc0a784423498ea59d663bd9ace29a92d1
HAROS <harchenk0s@yandex.ru>
2021-03-01 01:41:06 +0700

    Start project

---
+++ Planet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public float mass;
+    private float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
