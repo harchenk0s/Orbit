e32267da6a73f5db7718524e45cc6bc826fa3ae4
HAROS <harchenk0s@yandex.ru>
2021-03-01 03:33:24 +0700

    Add class-master and more structure class planet

--- Planet.cs
+++ Planet.cs
@@ -1,20 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public float mass;
-    private float speed;
+    public bool IsStatic;
+    public float Mass;
+    public float StartSpeed;
+    public Vector2 StartDirection;
+    public Guid ID;
+    public PlanetsMaster PlanetsMaster;
 
+    private float speed;
+    private Vector2 direction;
+    public bool isActive;
+    private bool calculationDone = false;
+    private Dictionary<Guid, Planet> interactions;
 
     void Start()
     {
-        
+        ID = Guid.NewGuid();
+        interactions = new Dictionary<Guid, Planet>();
     }
 
-    void Update()
+
+    public void Active(bool value)
     {
-        
+        if (value)
+        {
+            PlanetsMaster.ChangeDictionary(ID, this);
+        }
+        else
+        {
+            PlanetsMaster.ChangeDictionary(ID);
+        }
+        isActive = value;
+    }
+
+    public void ChangeDictionary(Dictionary<Guid, Planet> dict)
+    {
+        dict.Remove(ID);
+        interactions = dict;
     }
 }
---
+++ PlanetsMaster.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetsMaster : MonoBehaviour
+{
+    public Dictionary<Guid, Planet> InteractionsDictonary;
+
+    public void ChangeDictionary(Guid id)
+    {
+        if (InteractionsDictonary.ContainsKey(id))
+        {
+            InteractionsDictonary.Remove(id);
+            Planet planet;
+            foreach (var item in InteractionsDictonary)
+            {
+                planet = item.Value;
+                planet.ChangeDictionary(InteractionsDictonary);
+            }
+        }
+        
+    }
+
+    public void ChangeDictionary(Guid id, Planet pl)
+    {
+        if(!InteractionsDictonary.ContainsKey(id))
+        {
+            InteractionsDictonary.Add(id, pl);
+            Planet planet;
+            foreach (var item in InteractionsDictonary)
+            {
+                planet = item.Value;
+                planet.ChangeDictionary(InteractionsDictonary);
+            }
+        }
+        
+    }
+
+    private void Start()
+    {
+        InteractionsDictonary = new Dictionary<Guid, Planet>();
+    }
+
+
+}
