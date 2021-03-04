87e2f4e49b307f934842398a4c0b3b73677413c3
HAROS <harchenk0s@yandex.ru>
2021-03-04 18:11:34 +0700

    Start UI Control

---
+++ ControlPanel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPanel : MonoBehaviour
+{
+    private Dropdown dropdown;
+
+    private bool isStatic, isActive, isFocus, isTrailOn, isTrailsOneColor; 
+    private Planet[] planets;
+
+    private Planet ChoosenPlanet
+    {
+        get;
+        set;
+    }
+
+    void Start()
+    {
+        planets = FindObjectsOfType<Planet>();
+        foreach (Planet item in planets)
+        {
+            dropdown.options.Add(new Dropdown.OptionData { text = item.name });
+        }
+        ChoosenPlanet = planets[0];
+
+    }
+
+    void Add()
+    {
+
+    }
+
+    void Delete()
+    {
+
+    }
+
+    void ChangePlanet()
+    {
+
+    }
+
+    void ChangeColors()
+    {
+
+    }
+}
